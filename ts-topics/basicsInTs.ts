62a3e9518533b6555b3935d57dd3c91597d06bb1
Mohammed Kharoda <kharodawalam@gmail.com>
2023-01-02 12:40:20 +0530

    commit inital

---
+++ ts-topics/basicsInTs.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+let firstName: string = "John"; // string
+let age: number = 20; // number
+firstName = "Kerems";
+age = 85;
+
+let studentPrsetn:boolean // bool 
+let student:string[] // array of string
+
+let role: [number,string] // tuples
+role = [1,'kerm'] // tuples
+
