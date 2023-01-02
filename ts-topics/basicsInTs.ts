b25de42df48f8309e4f83c784317fde7b3107596
Mohammed-Propelius <kharodawalam@gmail.com>
2023-01-02 14:12:01 +0530

    inital comm

--- ts-topics/basicsInTs.ts
+++ ts-topics/basicsInTs.ts
@@ -8,7 +8,7 @@ let studentPrsetn: boolean; // bool
 let student: string[]; // array of string
 
 let role: [number, string]; // tuples
-role = [1, "kerms"]; // tuples
+role = [1, "ker"]; // tuples
 
 type Person = {
   personName: string;
