7a8a1ff4a3a229987f8586aaf6fec681d2f867f0
Mohammed-Propelius <kharodawalam@gmail.com>
2023-01-02 14:40:26 +0530

    basic Ts

--- ts-topics/basicsInTs.ts
+++ ts-topics/basicsInTs.ts
@@ -10,7 +10,62 @@ let student: string[]; // array of string
 let role: [number, string]; // tuples
 role = [1, "ker"]; // tuples
 
+// => Objects
 type Person = {
   personName: string;
   personNumber: number;
 };
+
+let info: Person = {
+  personName: "Mohammed",
+  personNumber: 8569,
+};
+
+//Unknow
+let transferData: unknown; // alternative of any
+
+// void or Never
+let fruitsData: (name: string) => never; // returns nothing
+let fruitsInfo: (name: string) => void; // return a void
+
+// Extening Types
+
+type vegs = {
+  veggieName: string;
+  veggieQuantity: number;
+};
+
+type fruits = vegs & {
+  // extending
+  fruitsName: string;
+  fruitsQuanity: number;
+};
+
+let basket: fruits = {
+  veggieName: "Carrots",
+  veggieQuantity: 12,
+  fruitsName: "apple",
+  fruitsQuanity: 25,
+};
+
+// Extending Interfaces
+interface book {
+  author: string;
+  awards: boolean;
+}
+
+
+// Extends the Markets
+interface market extends book {
+  soldCopy: number;
+  revenue: number;
+}
+
+let bookStore: market = {
+  author: "Jim Rayn",
+  awards: true,
+  soldCopy: 74,
+  revenue: 15233.21,
+};
+
+
