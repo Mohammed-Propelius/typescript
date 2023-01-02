3dcf82dc653ee24edfe5d0b4cae266c64a9bda8d
Mohammed Kharoda <kharodawalam@gmail.com>
2023-01-02 14:08:07 +0530

    init

--- ts-topics/basicsInTs.ts
+++ ts-topics/basicsInTs.ts
@@ -4,9 +4,13 @@ let age: number = 20; // number
 firstName = "Kerems";
 age = 85;
 
-let studentPrsetn:boolean // bool 
-let student:string[] // array of string
+let studentPrsetn: boolean; // bool
+let student: string[]; // array of string
 
-let role: [number,string] // tuples
-role = [1,'kerm'] // tuples
+let role: [number, string]; // tuples
+role = [1, "kerm"]; // tuples
 
+type Person = {
+  personName: string;
+  personNumber: number;
+};
